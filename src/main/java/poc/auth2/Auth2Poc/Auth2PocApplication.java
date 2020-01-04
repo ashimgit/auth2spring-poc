d3b5e52fb55844fff556dc3a4c5c42bf95db043a
ashimgit <ashimnb87@gmail.com>
2020-01-05 00:02:46 +0530

    Initial commit

---
+++ src/main/java/poc/auth2/Auth2Poc/Auth2PocApplication.java
@@ -0,0 +1,13 @@
+package poc.auth2.Auth2Poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth2PocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth2PocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poc/auth2/Auth2Poc/Auth2PocApplicationTests.java
@@ -0,0 +1,13 @@
+package poc.auth2.Auth2Poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth2PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
